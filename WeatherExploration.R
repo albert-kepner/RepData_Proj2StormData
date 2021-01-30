be025c660c07d97b93d5071704aca3890c47b868
Al Kepner <albert.kepner@outlook.com>
2021-01-30 14:24:48 -0500

    working

--- WeatherExploration.R
+++ WeatherExploration.R
@@ -42,6 +42,14 @@ years <- as.numeric(format(as.Date(StormFocus1$BGN_DATE, format="%d/%m/%Y"),"%Y"
 summary(years)
 
 
+years <- as.numeric(format(as.Date(sub("~","",StormFocus1$BGN_DATE), format="%d/%m/%Y"),"%Y"))
+
+summary(years)
+
+years <- sub(" 0:00:00","",as.character(StormFocus1$BGN_DATE)) %>% mdy()
+
+
+
 ## StormFocus2 <- StormFocus1 %>% 
 ##  mutate(year=as.numeric(format(as.Date(StormFocus1$BGN_DATE, format="%d/%m/%Y"),"%Y"))) %>%
 ##  mutate(harm_to_persons = FATALITIES + INJURIES) %>%
